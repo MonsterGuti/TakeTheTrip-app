394128ea36301af34170007f348242f6662c402c
MonsterGuti <gogulanovmartin@gmail.com>
2026-09-14 12:10:10 +0300

    Fix sitemap.xml view and resolve internal server error

--- tripApp/urls.py
+++ tripApp/urls.py
@@ -1,11 +1,32 @@
-import sitemaps
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
-from django.contrib.sitemaps.views import sitemap
-from django.contrib.sitemaps import Sitemap
-from django.urls import reverse
+from django.http import HttpResponse
+from django.urls import include, path
+
+
+# Функция за статичен sitemap.xml
+def sitemap_xml(request):
+    content = """<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://takethetripapp.com/</loc>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://takethetripapp.com/users/login/</loc>
+    <changefreq>monthly</changefreq>
+    <priority>0.5</priority>
+  </url>
+  <url>
+    <loc>https://takethetripapp.com/users/register/</loc>
+    <changefreq>monthly</changefreq>
+    <priority>0.5</priority>
+  </url>
+</urlset>"""
+    return HttpResponse(content, content_type="application/xml")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,26 +34,8 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('reviews/', include('reviews.urls')),
     path('notifications/', include('notifications.urls')),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('sitemap.xml', sitemap_xml, name='sitemap'),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
-
-
-class StaticViewSitemap(Sitemap):
-    priority = 1.0
-    changefreq = 'daily'
-
-    def items(self):
-        return ['home', 'login', 'register']  # имена на основните ти URL адреси
-
-    def location(self, item):
-        return reverse(item)
-
-
-sitemaps = {
-    'static': StaticViewSitemap,
-}
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
